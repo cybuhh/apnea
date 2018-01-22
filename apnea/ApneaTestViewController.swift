736b32aef2a1117e0c96e636ef6a6c52fbe13c18
Mateusz Cyburt <mateusz.cyburt@schibsted.pl>
2018-01-22 16:57:53 +0100

    added stack view

--- apnea/ApneaTestViewController.swift
+++ apnea/ApneaTestViewController.swift
@@ -10,24 +10,41 @@ import UIKit
 import MZTimerLabel
 
 class ApneaTestViewController: UIViewController {
-    
+    var stopwatch: MZTimerLabel!
+    var defaults: UserDefaults!
+
     private enum storeKeys: String {
         case historyApnea
     }
-    var stopwatch: MZTimerLabel!
-    var defaults: UserDefaults!
+    
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
     
     @IBOutlet weak var stoptimerLabel: UILabel!
     
     @IBAction func startButtonClicked(_ sender: UIButton) {
         stopwatch.start()
+        startButton.isHidden = true
+        stopButton.isHidden = false
+        saveButton.isHidden = true
+        resetButton.isHidden = true
     }
     @IBAction func stopButtonClicked(_ sender: UIButton) {
         stopwatch.pause()
+        startButton.isHidden = true
+        stopButton.isHidden = true
+        saveButton.isHidden = false
+        resetButton.isHidden = false
     }
     
     @IBAction func resetButtonClicked(_ sender: UIButton) {
         stopwatch.reset()
+        startButton.isHidden = false
+        stopButton.isHidden = true
+        saveButton.isHidden = true
+        resetButton.isHidden = true
     }
     
     @IBAction func saveButtonClicked(_ sender: UIButton) {
@@ -42,6 +59,10 @@ class ApneaTestViewController: UIViewController {
         super.viewDidLoad()
         defaults = UserDefaults(suiteName: "history")
         stopwatch = MZTimerLabel(label: stoptimerLabel)
+        startButton.isHidden = false
+        stopButton.isHidden = true
+        saveButton.isHidden = true
+        resetButton.isHidden = true
     }
     
     
