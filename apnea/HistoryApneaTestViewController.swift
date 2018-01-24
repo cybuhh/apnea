1af3b63a126e9ad8973393481783b34e2a1ec45b
cybuhh <>
2018-01-24 05:15:59 +0100

    added delegate and datasource for apneatest history

--- apnea/HistoryApneaTestViewController.swift
+++ apnea/HistoryApneaTestViewController.swift
@@ -14,13 +14,63 @@ struct ApneaHistoryCell {
 
 class ApneaHistoryViewModel {
     let rows = [ApneaHistoryCell(text: "test ApneaHistoryViewModel")]
+    
+    func setup() {
+        
+    }
+}
+
+class HistoryApneaTestViewControllerDataSource: NSObject, UITableViewDataSource {
+    let viewModel: ApneaHistoryViewModel
+    
+    init(viewModel: ApneaHistoryViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.rows.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HistoryApneTestCell", for: indexPath)
+        
+        let menuItem = viewModel.rows[indexPath.row]
+        cell.textLabel?.text = menuItem.text
+        
+        return cell
+    }
+}
+
+class HistoryApneaTestViewControllerDelegate: NSObject, UITableViewDelegate {
+    let viewModel: ApneaHistoryViewModel
+    let didSelectCallback: (String) -> ()
+    
+    init(viewModel: ApneaHistoryViewModel, didSelectCallback: @escaping (String) -> ()) {
+        self.viewModel = viewModel
+        self.didSelectCallback = didSelectCallback
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let menuItem = viewModel.rows[indexPath.row]
+        print(menuItem)
+        //self.didSelectCallback(menuItem.segueName)
+    }
 }
 
 class HistoryApneaTestViewController: UITableViewController {
     let viewModel = ApneaHistoryViewModel()
+    var viewControllerDataSource: HistoryApneaTestViewControllerDataSource?
+    var viewControllerDelegate: HistoryApneaTestViewControllerDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewControllerDataSource = HistoryApneaTestViewControllerDataSource(viewModel: viewModel)
+        viewControllerDelegate = HistoryApneaTestViewControllerDelegate(viewModel: viewModel, didSelectCallback: { [weak self] (segueName) in
+            self?.performSegue(withIdentifier: segueName, sender: nil)
+        })
+
+        tableView.dataSource = viewControllerDataSource
+        tableView.delegate = viewControllerDelegate
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
